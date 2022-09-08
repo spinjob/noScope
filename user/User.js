faa032c138a062e4e3c501e40c4d6f4a2db90d27
Spencer Johnson <spencerjohnson@spencers-mbp-2.myfiosgateway.com>
2022-09-08 17:04:40 -0400

    Deleting commented out mongoose-webhooks npm module use

--- user/User.js
+++ user/User.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
-//var mongooseWebhooks = require('mongoose-webhooks');
 var UserSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String
 });
-
-//UserSchema.plugin(mongooseWebhooks, {'urls': 'http://localhost:3000/users'})
 
 mongoose.model('User', UserSchema);
 
