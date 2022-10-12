b21821d0b3189744d622daef23ea5b5ec2ffc544
Spencer Johnson <spencerjohnson@Spencers-MacBook-Pro-2.local>
2022-10-12 17:19:36 +0200

    Updated Static File directory paths

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ const path = require('path');
 var InterfaceController = require('../interface/InterfaceController')
 var InterfaceEntityController = require('../interface_entity/InterfaceEntityController');
 
-//All other GET requests not handled will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 app.use(express.json({limit: "200mb", extended: true}))
 app.use(express.urlencoded({limit: "200mb", extended: true, parameterLimit: 50000}))
 app.use('/interfaces', InterfaceController);
 app.use('/interfaces', InterfaceEntityController);
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+//All other GET requests not handled will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 module.exports = app;   
