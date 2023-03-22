7df53d9664b9d9879bc95ddb9e31da5982c728d9
Spencer Johnson <spencerjohnson@macbook-pro.myfiosgateway.com>
2023-03-22 13:23:18 -0400

    Implemented create and update security scheme routes

--- models/interface_security_scheme/InterfaceSecuritySchemeController.js
+++ models/interface_security_scheme/InterfaceSecuritySchemeController.js
@@ -3,11 +3,42 @@ var router = express.Router();
 router.use(express.urlencoded({extended: true}));
 router.use(express.json());
 var InterfaceSecurityScheme = require('./InterfaceSecurityScheme');
+const crypto = require('crypto');
 
 // GET INTERFACE SECURITY SCHEMES
 router.post('/security', function(req,res){
     InterfaceSecurityScheme.find({parent_interface_uuid:{$in: req.body.interfaces}}, function (err, interfaceSecurityScheme) {
         if (err) return res.status(500).send("There was a problem finding security schemes for the provided interface IDs.");
+        res.status(200).send(interfaceSecurityScheme);
+    });
+});
+
+router.post('/:id/security', function(req,res){
+    InterfaceSecurityScheme.create({
+        uuid: crypto.randomUUID(),
+        parent_interface_uuid: req.params.id,
+        type: req.body.type,
+        flows: req.body.flows,
+        created_at: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+        updated_at: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    }, function (err,interfaceSecurityScheme) {
+        if (err) return res.status(500).send(err);
+        res.status(200).send(interfaceSecurityScheme);
+    });
+});
+
+router.put('/:id/security', function(req,res){
+    InterfaceSecurityScheme.findOneAndUpdate({
+        parent_interface_uuid: req.params.id,
+        security_scheme_uuid: req.body.security_scheme_uuid
+    }, {
+        $set: {
+            type: req.body.type,
+            flows: req.body.flows,
+            updated_at: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+        }
+    }, {new: true}, function(err,interfaceSecurityScheme) {
+        if (err) return res.status(500).send(err);
         res.status(200).send(interfaceSecurityScheme);
     });
 });
